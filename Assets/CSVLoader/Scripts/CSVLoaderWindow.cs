8184e02eebfc2999c3ac9f58166b613de3355689
heartSora <541131938@qq.com>
2019-01-16 13:34:38 +0800

    update

--- Assets/CSVLoader/Scripts/CSVLoaderWindow.cs
+++ Assets/CSVLoader/Scripts/CSVLoaderWindow.cs
@@ -3,6 +3,7 @@
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Sora.Tools.CSVLoader
 {
@@ -31,8 +32,8 @@ namespace Sora.Tools.CSVLoader
         /// 边距
         /// </summary>
         private const float Margin = 10f;
-        private const float LINE_HEIGHT = 26f;
-        private const float BUTTON_HEIGHT = 20f;
+        private const float LINE_HEIGHT = 20f;
+        private const float BUTTON_HEIGHT = 18f;
         /// <summary>
         /// 已经加载的文件路径
         /// </summary>
@@ -40,6 +41,7 @@ namespace Sora.Tools.CSVLoader
         /// <returns></returns>
         private HashSet<string> loadedFilePath = new HashSet<string>();
         private Vector2 inputScrollPosition;
+        private float inputHeight;
         [MenuItem("Sora/Tools/CSVLoader")]
         static void Open()
         {
@@ -59,49 +61,113 @@ namespace Sora.Tools.CSVLoader
             var inputArea = new Rect(Margin / 2, Margin / 2, position.width - previewMinWidth - Margin - Margin - splitLineWidth, position.height - Margin);
             var splitLineArea = new Rect(inputArea.xMax + Margin / 2, 0, splitLineWidth, position.height);
             EditorGUI.DrawRect(splitLineArea, new Color(0, 0, 0, 0.6f));
-            EditorGUI.DrawRect(inputArea, Color.cyan);
             var scrollViewOffsetHeight = 10f;
             var scrollRect = new Rect(inputArea.x, inputArea.y, inputArea.width, inputArea.height - Margin - splitLineWidth - BUTTON_HEIGHT);
             var scrollViewRect = new Rect(inputArea.x, inputArea.y - scrollViewOffsetHeight, inputArea.width, createDataMap.Count * LINE_HEIGHT + scrollViewOffsetHeight);
             inputScrollPosition = GUI.BeginScrollView(scrollRect, inputScrollPosition, scrollViewRect, false, false, GUIStyle.none, GUIStyle.none);
-            var dataIndex = 0;
+            inputHeight = 0f;
             foreach (var data in createDataMap.Values)
             {
-                var filePathRect = new Rect(inputArea.x, inputArea.y + dataIndex * LINE_HEIGHT, 200, LINE_HEIGHT);
-                var filePathButtonRect = new Rect(filePathRect.xMax, 0, 80f, BUTTON_HEIGHT);
-                EditorGUI.LabelField(filePathRect, "CSV文件路径", data.loadFilePath);
-                if (GUI.Button(filePathButtonRect, "选择CSV"))
+                var buttonWidth = 80f;
+                var fieldWidth = inputArea.width * .8f;
+                var foldoutRect = new Rect(inputArea.x, inputHeight, fieldWidth, LINE_HEIGHT);
+                var removeButtonRect = new Rect(foldoutRect.xMax + Margin, inputHeight, buttonWidth, BUTTON_HEIGHT);
+                var foldoutTitle = "未选择文件";
+                if (!string.IsNullOrEmpty(data.loadFilePath))
                 {
-                    var selectedPath = EditorUtility.OpenFilePanelWithFilters("选择文件", "", new string[] { "CSV文件", "csv" });
-                    if (loadedFilePath.Contains(selectedPath))
-                    {
-                        Debug.LogError("文件已经被加载");
-                        continue;
-                    }
-                    var saveFilePath = data.loadFilePath;
-                    if (!string.IsNullOrEmpty(saveFilePath) && loadedFilePath.Contains(saveFilePath)) loadedFilePath.Remove(saveFilePath);
-                    data.loadFilePath = saveFilePath;
+                    foldoutTitle = new FileInfo(data.loadFilePath).Name.Split('.')[0];
                 }
-                dataIndex++;
+                data.foldout = EditorGUI.Foldout(foldoutRect, data.foldout, foldoutTitle, true);
+                if (GUI.Button(removeButtonRect, "x"))
+                {
+                    createDataMap.Remove(data.createIndex);
+                    break;
+                }
+                inputHeight += LINE_HEIGHT;
+                if (data.foldout)
+                {
+                    EditorGUI.indentLevel++;
+                    var filePathRect = new Rect(inputArea.x, inputHeight, fieldWidth, LINE_HEIGHT);
+                    var filePathButtonRect = new Rect(filePathRect.xMax + Margin, inputHeight, buttonWidth, BUTTON_HEIGHT);
+                    EditorGUI.BeginDisabledGroup(true);
+                    EditorGUI.TextField(filePathRect, "选择路径", data.loadFilePath);
+                    EditorGUI.EndDisabledGroup();
+                    if (GUI.Button(filePathButtonRect, "选择CSV"))
+                    {
+                        var selectedPath = EditorUtility.OpenFilePanelWithFilters("选择文件", "", new string[] { "CSV文件", "csv" });
+                        if (loadedFilePath.Contains(selectedPath))
+                        {
+                            Debug.LogError("文件已经被加载");
+                            continue;
+                        }
+                        var saveFilePath = data.loadFilePath;
+                        if (!string.IsNullOrEmpty(saveFilePath) && loadedFilePath.Contains(saveFilePath)) loadedFilePath.Remove(saveFilePath);
+                        data.loadFilePath = selectedPath;
+                    }
+                    AddHeight(1);
+                    EditorGUI.BeginDisabledGroup(true);
+                    var createTypeRect = new Rect(inputArea.x, inputHeight, fieldWidth, LINE_HEIGHT);
+                    EditorGUI.EnumPopup(createTypeRect, "数据类型", data.createType);
+                    EditorGUI.EndDisabledGroup();
+                    var previewButtonRect = new Rect(createTypeRect.xMax + Margin, inputHeight, buttonWidth, BUTTON_HEIGHT);
+                    EditorGUI.BeginDisabledGroup(string.IsNullOrEmpty(data.loadFilePath));
+                    if (GUI.Button(previewButtonRect, "预览脚本"))
+                    {
+                        PreviewScript(data);
+                    }
+                    EditorGUI.EndDisabledGroup();
+                    AddHeight(0);
+                    if (!string.IsNullOrEmpty(data.loadFilePath))
+                    {
+                        AddSpace();
+                        var scriptRect = new Rect(inputArea.x, inputHeight, fieldWidth, LINE_HEIGHT);
+                        var scriptButtonRect = new Rect(scriptRect.xMax + Margin, inputHeight, buttonWidth, BUTTON_HEIGHT);
+                        var fileInfo = new FileInfo(data.loadFilePath);
+                        data.scriptNameSpace = EditorGUI.TextField(scriptRect, "namespace", data.scriptNameSpace);
+                        if (GUI.Button(scriptButtonRect, "脚本路径"))
+                        {
+                            var saveScriptFilePath = EditorUtility.SaveFilePanel("保存脚本", "", $"{fileInfo.Name.Split('.')[0]}.cs", "cs");
+                            data.scriptFilePath = saveScriptFilePath;
+                        }
+                        AddHeight(1);
+                        var resourceRect = new Rect(inputArea.x, inputHeight, fieldWidth, LINE_HEIGHT);
+                        var resourceButtonRect = new Rect(resourceRect.xMax + Margin, inputHeight, buttonWidth, BUTTON_HEIGHT);
+                        EditorGUI.BeginDisabledGroup(true);
+                        EditorGUI.TextField(resourceRect, "资源路径", data.resourceFilePath);
+                        EditorGUI.EndDisabledGroup();
+                        if (GUI.Button(resourceButtonRect, "资源路径"))
+                        {
+                            var saveResourceFilePath = EditorUtility.SaveFilePanel("保存资源", data.scriptFilePath, $"{fileInfo.Name.Split('.')[0]}.cs", "cs");
+                            data.resourceFilePath = saveResourceFilePath;
+                        }
+                        AddHeight(0);
+                    }
+                    EditorGUI.indentLevel--;
+                }
             }
             GUI.EndScrollView();
-            var lastRectHeight = 0f;
-            if (scrollViewRect.yMax > scrollRect.yMax)
+            AddSpace(10);
+            var lastRectHeight = inputHeight;
+            if (lastRectHeight > scrollRect.yMax)
             {
                 lastRectHeight = scrollRect.yMax;
-            }
-            else
-            {
-                lastRectHeight = scrollViewRect.yMax;
             }
             splitLineArea = new Rect(inputArea.x, lastRectHeight + Margin / 2, inputArea.width, splitLineWidth);
             EditorGUI.DrawRect(splitLineArea, new Color(0, 0, 0, 0.6f));
             var addButtonRect = new Rect(inputArea.x, splitLineArea.yMax + Margin / 2, 50, BUTTON_HEIGHT);
+            var generateButtonRect = new Rect(addButtonRect.xMax + Margin, splitLineArea.yMax + Margin / 2, 50, BUTTON_HEIGHT);
             if (GUI.Button(addButtonRect, "+"))
             {
-                createDataMap.Add(CreateIndex, new CreateData(CreateIndex));
+                createDataMap.Add(CreateIndex, new CreateData(CreateIndex) { foldout = true });
                 CreateIndex++;
             }
+            var createEnable = createDataMap.Count > 0;
+            foreach (var data in createDataMap.Values) createEnable &= data.prepareComplete;
+            EditorGUI.BeginDisabledGroup(!createEnable);
+            if (GUI.Button(generateButtonRect, "创建"))
+            {
+            }
+            EditorGUI.EndDisabledGroup();
             #endregion 输入区域
 
             #region 预览区域
@@ -109,6 +175,23 @@ namespace Sora.Tools.CSVLoader
             EditorGUI.DrawRect(previewArea, new Color(0, 0, 0, 0.4f));
             #endregion 预览区域
         }
+
+        /// <summary>
+        /// 预览脚本
+        /// </summary>
+        /// <param name="data"></param>
+        private void PreviewScript(CreateData data)
+        {
+
+        }
+
+        private void AddHeight(int space)
+        {
+            inputHeight += LINE_HEIGHT;
+            inputHeight += space;
+        }
+        private void AddSpace(int height = 1) => inputHeight += height;
+
         private static int CreateIndex = int.MinValue;
     }
 }
--- Assets/CSVLoader/Scripts/CreateData.cs
+++ Assets/CSVLoader/Scripts/CreateData.cs
@@ -1,5 +1,8 @@
 //Author: sora
 
+using System.IO;
+using UnityEngine;
+
 namespace Sora.Tools.CSVLoader
 {
     /// <summary>
@@ -22,7 +25,15 @@ namespace Sora.Tools.CSVLoader
         /// <summary>
         /// 文件位置(csv文件位置)
         /// </summary>
-        public string loadFilePath;
+        public string loadFilePath
+        {
+            get => m_loadFilePath;
+            set
+            {
+                m_loadFilePath = value;
+                GenerateScriptContent();
+            }
+        }
         /// <summary>
         /// 创建类型
         /// </summary>
@@ -32,13 +43,45 @@ namespace Sora.Tools.CSVLoader
         /// </summary>
         public string scriptFilePath;
         /// <summary>
+        /// 脚本命名空间
+        /// </summary>
+        public string scriptNameSpace;
+        /// <summary>
         /// 资源文件位置(选择保存位置)
         /// </summary>
         public string resourceFilePath;
         public int createIndex;
+        public bool foldout;
+        /// <summary>
+        /// 资源准备就绪,可生成文件了
+        /// </summary>
+        public bool prepareComplete
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(scriptFilePath) && !string.IsNullOrEmpty(resourceFilePath);
+            }
+        }
+        /// <summary>
+        /// 脚本代码内容
+        /// </summary>
+        public string scriptContent;
+        private string m_loadFilePath;
         public CreateData(int createIndex)
         {
             this.createIndex = createIndex;
+        }
+        private void GenerateScriptContent()
+        {
+            初始化scriptContent
+            
+            var fileStream = new FileStream(loadFilePath, FileMode.Open, FileAccess.Read);
+            var streamReader = new StreamReader(fileStream);
+            var line = "";
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                Debug.Log(line);
+            }
         }
     }
 }
